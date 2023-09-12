44bb48172c9813cf56ad52ca8d2c73e47232fc0d
Gonçalo <gcr1994@gmail.com>
2023-09-12 17:12:33 +0100

    first commit

---
+++ src/utils/api.ts
@@ -0,0 +1,34 @@
+import axios, { AxiosRequestConfig } from 'axios'
+
+export const httpClient = axios.create();
+
+/**
+ * Use Axios to make a get request
+ * @param url request url
+ * @param header header options
+ * @returns {Promise<AxiosRequestConfig<any>>}
+ */
+export const get = async  (url: string, header?: AxiosRequestConfig): Promise<AxiosRequestConfig<any>> => {
+   return axios.get(url, header);
+}
+
+/**
+ * Use Axios to make a post request
+ * @param url request url
+ * @param body request body
+ * @param header header options
+ * @returns {Promise<AxiosRequestConfig<any>>}
+ */
+export const post = async (url:string, body?:AxiosRequestConfig<any> , header?: AxiosRequestConfig): Promise<AxiosRequestConfig<any>> => {
+    return axios.post(url, body, header)
+}
+
+/**
+ * Use Axios to make a delete request
+ * @param url request url
+ * @param header header options
+ * @returns {Promise<AxiosRequestConfig<any>>}
+ */
+export const remove = async (url: string, header?:AxiosRequestConfig): Promise<AxiosRequestConfig<any>> => {
+    return axios.delete(url,header)
+}
