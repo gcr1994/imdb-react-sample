b9ca6a7dd2145282c49389ccd7ea59b20b1aa364
Gonçalo <gcr1994@gmail.com>
2023-09-29 14:25:55 +0100

    Improving visuals

--- src/pages/profile.tsx
+++ src/pages/profile.tsx
@@ -2,24 +2,25 @@ import { putUser } from "@/api/authentication";
 import ImageDropzone from "@/components/ImageDropzone";
 import { User } from "@/types/user";
 import useStore, { Store } from "@/utils/store";
-import AspectRatio from "@mui/joy/AspectRatio";
-
-import {
-  Card,
-  Box,
-  Divider,
-  FormControl,
-  Input,
-  Stack,
-  CardActions,
-  FormLabel,
-  Button,
-} from "@mui/material";
-import Typography from "@mui/joy/Typography";
-import Image from "next/image";
 import { useCallback, useEffect, useState } from "react";
+import * as React from "react";
+import AspectRatio from "@mui/joy/AspectRatio";
+import Box from "@mui/joy/Box";
+import Button from "@mui/joy/Button";
+import Divider from "@mui/joy/Divider";
+import FormControl from "@mui/joy/FormControl";
+import FormLabel from "@mui/joy/FormLabel";
+import Input from "@mui/joy/Input";
+import IconButton from "@mui/joy/IconButton";
+import Stack from "@mui/joy/Stack";
+import Typography from "@mui/joy/Typography";
+import Card from "@mui/joy/Card";
+import CardActions from "@mui/joy/CardActions";
+import CardOverflow from "@mui/joy/CardOverflow";
 
-const Profile = () => {
+import EditRoundedIcon from "@mui/icons-material/EditRounded";
+
+export default function Profile() {
   const store: Store = useStore();
   const [user, setUser] = useState<User>({} as User);
   const [binFile, setBinFile] = useState<File>({} as File);
@@ -35,20 +36,6 @@ const Profile = () => {
     setBinFile(files[0]);
   }, []);
 
-  const getBase64 = async (file: File | Blob): Promise<string> => {
-    if (file === null) Promise.reject();
-    return new Promise((resolve, reject) => {
-      const reader = new FileReader();
-      reader.readAsDataURL(file!);
-      reader.onload = () => {
-        return resolve(reader.result as string);
-      };
-      reader.onerror = (err) => {
-        return reject(err);
-      };
-    });
-  };
-
   const onSubmit = async () => {
     try {
       if (!user) {
@@ -63,14 +50,12 @@ const Profile = () => {
   };
 
   return (
-    <>
-      <h1>Your Profile</h1>
-      <form onSubmit={onSubmit}>
-        <ImageDropzone onDrop={onDrop} multiple={false}></ImageDropzone>
-
-        <button> Save </button>
-      </form>
-
+    <Box
+      sx={{
+        flex: 1,
+        width: "100%",
+      }}
+    >
       <Stack
         spacing={4}
         sx={{
@@ -90,58 +75,147 @@ const Profile = () => {
         <Card>
           <Box sx={{ mb: 1 }}>
             <Typography level="title-md">Personal info</Typography>
-            <Typography level="body-sm">View your profile here! </Typography>
-            {user.image ? (
+            <Typography level="body-sm">
+              Customize how your profile information will apper to the networks.
+            </Typography>
+          </Box>
+          <Divider />
+          <Stack
+            direction="row"
+            spacing={3}
+            sx={{ display: { xs: "none", md: "flex" }, my: 1 }}
+          >
+            <Stack direction="column" spacing={1}>
+              <AspectRatio
+                ratio="1"
+                maxHeight={200}
+                sx={{ flex: 1, minWidth: 120, borderRadius: "100%" }}
+              >
+                <img src={user?.image || ""} loading="lazy" alt="" />
+              </AspectRatio>
+              <IconButton
+                aria-label="upload new picture"
+                size="sm"
+                variant="outlined"
+                color="neutral"
+                sx={{
+                  bgcolor: "background.body",
+                  position: "absolute",
+                  zIndex: 2,
+                  borderRadius: "50%",
+                  left: 100,
+                  top: 170,
+                  boxShadow: "sm",
+                }}
+              >
+                {
+                  //TODO use ImageDropZone
+                }
+                <EditRoundedIcon />
+              </IconButton>
+            </Stack>
+            <Stack spacing={2} sx={{ flexGrow: 1 }}>
+              <Stack spacing={1}>
+                <FormLabel>Email</FormLabel>
+                <FormControl
+                  sx={{
+                    display: {
+                      sm: "flex-column",
+                      md: "flex-row",
+                    },
+                    gap: 2,
+                  }}
+                >
+                  <Input
+                    size="sm"
+                    placeholder="Email"
+                    value={user.email}
+                    disabled
+                  />
+                </FormControl>
+              </Stack>
+              <Stack direction="row" spacing={2}></Stack>
+            </Stack>
+          </Stack>
+          <Stack
+            direction="column"
+            spacing={2}
+            sx={{ display: { xs: "flex", md: "none" }, my: 1 }}
+          >
+            <Stack direction="row" spacing={2}>
               <Stack direction="column" spacing={1}>
                 <AspectRatio
                   ratio="1"
-                  maxHeight={240}
-                  sx={{ flex: 1, minWidth: 240, borderRadius: "100%" }}
+                  maxHeight={108}
+                  sx={{ flex: 1, minWidth: 108, borderRadius: "100%" }}
                 >
-                  <Image
-                    src={user.image}
-                    width={240}
-                    height={240}
-                    alt="User image"
-                    priority
-                  ></Image>
+                  <img src={user?.image || ""} loading="lazy" alt="" />
                 </AspectRatio>
+                <IconButton
+                  aria-label="upload new picture"
+                  size="sm"
+                  variant="outlined"
+                  color="neutral"
+                  sx={{
+                    bgcolor: "background.body",
+                    position: "absolute",
+                    zIndex: 2,
+                    borderRadius: "50%",
+                    left: 85,
+                    top: 180,
+                    boxShadow: "sm",
+                  }}
+                >
+                  {
+                    //TODO use ImageDropZone
+                  }
+                  <EditRoundedIcon />
+                </IconButton>
               </Stack>
-            ) : null}
-          </Box>
-          <Divider />
-          <Stack spacing={2} sx={{ flexGrow: 1 }}>
-            <Stack spacing={1}>
-              <FormLabel>Email</FormLabel>
-              <FormControl
-                sx={{
-                  display: {
-                    sm: "flex-column",
-                    md: "flex-row",
-                  },
-                  gap: 2,
-                }}
-              >
-                <Input
-                  size="small"
-                  disabled
-                  value={user.email}
-                  placeholder="Email"
-                  sx={{ flexGrow: 1 }}
-                />
-              </FormControl>
+              <Stack spacing={1} sx={{ flexGrow: 1 }}>
+                <FormLabel>Email</FormLabel>
+                <FormControl
+                  sx={{
+                    display: {
+                      sm: "flex-column",
+                      md: "flex-row",
+                    },
+                    gap: 2,
+                  }}
+                >
+                  <Input
+                    size="sm"
+                    placeholder="Email"
+                    value={user.email}
+                    disabled
+                  />
+                </FormControl>
+              </Stack>
             </Stack>
           </Stack>
-          <CardActions sx={{ alignSelf: "flex-end", pt: 2 }}>
-            <Button size="small" variant="outlined">
-              Cancel
-            </Button>
-            <Button size="small">Save</Button>
-          </CardActions>
+          <CardOverflow sx={{ borderTop: "1px solid", borderColor: "divider" }}>
+            <CardActions sx={{ alignSelf: "flex-end", pt: 2 }}>
+              <Button size="sm" variant="outlined" color="neutral">
+                Cancel
+              </Button>
+              <Button onSubmit={onSubmit} size="sm" variant="solid">
+                Save
+              </Button>
+            </CardActions>
+          </CardOverflow>
+        </Card>
+        <Card>
+          <Box sx={{ mb: 1 }}>
+            <Typography level="body-sm">Favorite movies</Typography>
+          </Box>
+          <Divider />
+          <Stack spacing={2} sx={{ my: 1 }}>
+            {
+              //TODO Favorite List
+            }
+          </Stack>
         </Card>
       </Stack>
-    </>
+    </Box>
   );
-};
-
-export default Profile;
+}
