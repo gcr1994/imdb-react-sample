2b6ff0accf2b736a99a2fadb5c65bfa142d10d98
Gonçalo <gcr1994@gmail.com>
2023-10-18 13:33:11 +0100

    Fixing import

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,7 +2,7 @@ import ButtonBar from "@/components/ButtonBar";
 import React from "react";
 import { Hydrate, QueryClient, QueryClientProvider } from "react-query";
 // These styles apply to every route in the application
-import "./globals.css";
+import "../app/globals.css";
 
 export default function MyApp({
   Component,
